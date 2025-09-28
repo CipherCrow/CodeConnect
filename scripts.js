dea540f0e3f2746bd0aac7de451aeb134ae39a23
CipherCrow <jonastorquatro@gmail.com>
2025-09-28 19:32:31 -0300

    ler arquivo async

--- scripts.js
+++ scripts.js
@@ -5,3 +5,36 @@ uploadBtn.addEventListener("click", () =>
 {
     inputUpload.click()
 })
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve,reject) => {
+        const leitor = new FileReader()
+        leitor.onload = () => {
+            resolve({url : leitor.result, nome: arquivo.name})
+        }
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+
+        leitor.readAsDataURL(arquivo)
+    } )
+}
+
+const imagemPrincipal = document.querySelector(".main-image")
+const nomeDaImagem = document.querySelector(".container-imagem-nome p")
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0]
+    
+    if(arquivo){
+        try{
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo) 
+            imagemPrincipal.src = conteudoDoArquivo.url
+            nomeDaImagem.textContent = conteudoDoArquivo.nome
+        }catch(erro){
+            console.error("Deu ruim")
+        }
+    }
+
+})
